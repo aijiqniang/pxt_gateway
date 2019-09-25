15abe694f9eb1a702e7ad96a599288f570928b4d
tulane <dpzhuming@szeastroc.com>
2019-09-25 13:56:34 +0800

    Revert "请求头打印"
    This reverts commit 233f3784eeb036056e4d69760d11cae93f80ae88.

--- src/main/java/com/szeastroc/gateway/filter/addResponseHeaderFilter.java
+++ src/main/java/com/szeastroc/gateway/filter/addResponseHeaderFilter.java
@@ -2,23 +2,15 @@ package com.szeastroc.gateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.szeastroc.common.constant.CookieConstant;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.Collection;
-import java.util.Enumeration;
 import java.util.UUID;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
 
-@Slf4j
 @Component
 public class addResponseHeaderFilter extends ZuulFilter{
     @Override
@@ -41,23 +33,6 @@ public class addResponseHeaderFilter extends ZuulFilter{
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletResponse response = requestContext.getResponse();
         response.setHeader("X-Foo", UUID.randomUUID().toString());
-
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
-                .getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-
-        /**
-         * 传递请求头
-         */
-        Enumeration<String> headerNames = request.getHeaderNames();
-        if (headerNames != null) {
-            while (headerNames.hasMoreElements()) {
-                String name = headerNames.nextElement();
-                String values = request.getHeader(name);
-                log.info("gateway请求头key:" + name + ",value:" + values);
-
-            }
-        }
         return null;
     }
 }
