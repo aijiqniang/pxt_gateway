a456189adcb01039b1e5a68aa6405b81135ee98e
chenchao <chenchao@szeastroc.com>
2021-01-08 14:05:03 +0800

    fix:鹏讯通集成knife4j

--- src/main/java/com/szeastroc/gateway/Application.java
+++ src/main/java/com/szeastroc/gateway/Application.java
@@ -1,11 +1,18 @@
 package com.szeastroc.gateway;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import com.github.xiaoymin.knife4j.spring.filter.ProductionSecurityFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by Tulane
@@ -13,13 +20,18 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 @EnableFeignClients({"com.szeastroc"})
 @ComponentScan({"com.szeastroc"})
+@EnableSwagger2
+@EnableKnife4j
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("service start success");
     }
+
 }
